0171b3586763b4dc636f5b295fc31398744532fa
Jose Maria <josemariarcha@gmail.com>
2026-03-12 19:52:11 +0100

    Solved lab

--- index.js
+++ index.js
@@ -8,10 +8,9 @@ const s4 = "bread";
 const s5 = "and";
 
 // Concatenate the string variables into one new string
-
-
+const tongueTwister = s1 + " " + s2 + " " + s3 + " " + s4 + " " + s5 + " " + s3 + " " + s2 + " " + s1 + " " + s4;
 // Print out the concatenated string
-
+console.log(tongueTwister);
 
 
 
@@ -22,10 +21,10 @@ const part1 = "java";
 const part2 = "script";
 
 // Convert the last letter of part1 and part2 to uppercase and concatenate the strings
-
+const result = part1.slice(0 , part1.length - 1) + part1.slice(part1.length - 1).toUpperCase() + part2.slice(0 , part2.length - 1) + part2.slice(part2.length - 1).toUpperCase();
 
 // Print the cameLtaiL-formatted string
-
+console.log(result);
 
 
 
@@ -35,10 +34,10 @@ const part2 = "script";
 const billTotal = 84;
 
 // Calculate the tip (15% of the bill total)
-
+const tipAmount = (billTotal * 15)/100;
 
 // Print out the tipAmount
-
+console.log(tipAmount);
 
 
 
@@ -47,11 +46,10 @@ const billTotal = 84;
 *******************************************/
 
 // Generate a random integer between 1 and 10 (inclusive)
-
+const randomNumber = Math.floor(Math.random() * 10) + 1 ;
 
 // Print the generated random number
-
-
+console.log(randomNumber);
 
 /*******************************************
     Iteration 3.1 | Booleans
@@ -74,3 +72,8 @@ const expression5 = !a || !b;
 const expression6 = !(a || b);
 
 const expression7 = a && a;
+
+const predictions = "false, true, false, true, true, false, true";
+const results = expression1 + " " + expression2 + " " + expression3 + " " + expression4 + " " + expression5 + " " + expression6 + " " + expression7;
+console.log(predictions);
+console.log(results);
